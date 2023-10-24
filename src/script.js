05e33deb3bce2713ddbdb3af0458a4da671f609a
st33ze <st33ze@protonmail.com>
2023-10-24 15:02:40 +0200

    feat(script): add animation off to side menu on resize

--- src/script.js
+++ src/script.js
@@ -11,13 +11,21 @@ searchButton.addEventListener('click', () => {
 searchCloseButton.addEventListener('click', () => {
   searchContainer.classList.remove('search-container-show');
 });
-// Turn off animation on screen resize.
-window.addEventListener('resize', () => {
-  searchContainer.classList.add('animation-off');
-});
+
+/* ========== SIDE MENU ========== */
 // Toggle side menu on small screens.
 const menuButton = header.querySelector('button[aria-label="Menu"]');
 const menu = document.getElementById('side-menu');
 const closeMenuButton = menu.querySelector('button[aria-label="Close menu"]');
-menuButton.addEventListener('click', () => menu.classList.add('side-menu-show'));
+menuButton.addEventListener('click', () => {
+  menu.classList.remove('animation-off');
+  menu.classList.add('side-menu-show');
+});
 closeMenuButton.addEventListener('click', () => menu.classList.remove('side-menu-show'));
+
+/* ========== UTILITY ========== */
+// Turn off animation on screen resize.
+window.addEventListener('resize', () => {
+  searchContainer.classList.add('animation-off');
+  menu.classList.add('animation-off');
+});
